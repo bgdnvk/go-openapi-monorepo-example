b6890488d871607659455937a471b81de9d61ad4
bgdnvk <bnovykov@gmail.com>
2023-08-11 11:48:00 +0200

    err handler

--- services/genexample/main.go
+++ services/genexample/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"log"
 	"net/http"
 
 	"github.com/bgdnvk/go-openapi-monorepo-example/services/genexample/gen"
@@ -11,12 +12,20 @@ func main() {
 	r := chi.NewRouter()
 
 	server := &Server{}
+
 	middlewares := []gen.MiddlewareFunc{
 		loggingMiddleware,
 	}
+
+	errorHandler := func(w http.ResponseWriter, r *http.Request, err error) {
+		log.Printf("err handling req %v", err)
+		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+	}
+
 	wrapper := &gen.ServerInterfaceWrapper{
 		Handler:            server,
 		HandlerMiddlewares: middlewares,
+		ErrorHandlerFunc:   errorHandler,
 	}
 
 	r.Get("/hello", wrapper.GetHello)
